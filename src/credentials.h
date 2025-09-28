c9a2fd63f23c10c31a0f257395ecec4f4da551cc
ikr <ingo.kreuz@gmx.net>
2025-09-28 22:42:27 +0200

    Stand vor Umstellung auf viertel-Stündlich. Gegenüber Original-Projekt geändert: - grafische Oberfläche mit SVG in HTML eingebettet - vereinfachte Logik, um Ein-/Ausschaltungen zu berechnen

--- src/credentials.h
+++
@@ -1,5 +0,0 @@
-//do not publish this file on github!!!
-//use .gitignore to exclude from upload
-const char *ssid = "mySSID";         //--> replace with your own ssid and put it in a file credentials.h
-const char *password = "myPassword"; //--> replace with your own wifi password and put it in a file credentials.h
-const char *token = "3A77EECF61BD445F47241A5A36202185C35AF3AF58609E19B53F3A8872AD7BE1-1"; // API Tibber, Demo Token --> replace with your own token and put it in a file credentials.h
